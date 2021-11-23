bc09e83a3cfe6693b65e5bf6ec621c52d97cc2d3
bugbytes-io <bugbytes.io@gmail.com>
2021-11-23 10:08:55 +0000

    #2 Fetching NASA Images and Saving to Filesystem

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+from pathlib import Path
+
 API_KEY = ""  # enter your key here
 API_URL = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
+
+IMAGE_DIR = Path() / 'images'
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from io import BytesIO
+import os
 import typer
+import requests
 from datetime import datetime
+from config import API_URL, IMAGE_DIR
+from PIL import Image
 
 app = typer.Typer()
-
-@app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
 
 default_date = typer.Argument(
     datetime.now().strftime('%Y-%m-%d'),
@@ -13,8 +14,39 @@ default_date = typer.Argument(
 )
 
 @app.command()
-def day_of_week(date: datetime = default_date):
-    typer.echo(date.strftime('%A'))
+def fetch_image(date: datetime = default_date, save: bool = False):
+    print("Sending API request...")
+    dt = str(date.date())
+
+    # add the 'date' query parameter to the NASA API call
+    url_for_date = f"{API_URL}&date={dt}"
+    response = requests.get(url_for_date)
+
+    # raise error if request fails
+    response.raise_for_status()
+
+    # extract url and title from JSON response
+    data = response.json()
+    url = data['url']
+    title = data['title']
+
+    # fetch the Image from the url, and create PIL.Image object
+    print("Fetching Image...")
+    image_response = requests.get(url)
+    image = Image.open(BytesIO(image_response.content))
+
+    # show image on user's desktop
+    image.show()
+
+    # save image to filesystem if save flag is True
+    if save:
+        if not IMAGE_DIR.exists():
+            os.mkdir(IMAGE_DIR)
+        image_name = f"{title}.{image.format}"
+        image.save(IMAGE_DIR / image_name, image.format)
+
+    image.close()
+    
 
 if __name__ == '__main__':
     app()
